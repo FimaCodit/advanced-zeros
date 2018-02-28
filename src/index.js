55ffbc0950e7b190fbe3b31fc373e73d62c34ea4
Efim Poleschuk <limitlessFIMA@gmail.com>
2018-02-28 11:51:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let b,a = 0,c,num,w;
+   num = number;
+   b = base;
+   for(let i = 2; i <= base; i++){
+     if(b % i == 0){
+       while(b % i == 0){
+         a++;
+         b = Math.floor(b/i);
+     }
+       w = 0;
+       c = number;
+       while (c/i > 0){
+         w+=Math.floor(c/i);
+         c = Math.floor(c/i);
+     }
+     if(num > w/a){ 
+       num=w/a;
+     } 
+   }
+ }
+ return Math.floor(num);
 }
